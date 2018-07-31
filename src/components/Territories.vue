280dc935c15853a972787370f74d51a1c6914bc2
Ryan Tercias <ryan.tercias@Ryans-MacBook-Pro-3.local>
2018-07-31 00:04:05 -0400

    adds basic checkout/checkin functionality

--- src/components/Territories.vue
+++ src/components/Territories.vue
@@ -2,7 +2,7 @@
   <div class="territories">
     <header class="row align-items-center justify-content-between">
       <h4>Service Group: {{groupCode}}</h4>
-      <b-dropdown right>
+      <b-dropdown right variant="secondary">
         <span slot="button-content">{{availability}}</span>
         <b-dropdown-item v-for="avail in availabilityFilters" v-bind:key="avail" @click="setAvailability(avail)">
           <font-awesome-icon icon="check" v-if="availability === avail" /> {{avail}}
@@ -10,10 +10,19 @@
       </b-dropdown>
     </header>
     <b-list-group class="flex-row flex-wrap">
-      <b-list-group-item v-for="terr in territories" v-bind:key="terr.id" data-toggle="collapse" class="col-md-6">
-        <b-link :to="`/territories/${groupCode}/${terr.id}`">
-          {{terr.name}}<span v-if="terr.city"> - {{terr.city}}</span>
-        </b-link>
+      <b-list-group-item v-for="terr in territories" v-bind:key="terr.id" data-toggle="collapse" class="territory-card col-md-6">
+        <div class="row justify-content-between">
+          <b-link :to="`/territories/${groupCode}/${terr.id}`">
+            <h5>{{terr.name}}<span v-if="terr.city"> - {{terr.city}}</span></h5>
+          </b-link>
+          <div class="btn-group" role="group" aria-label="Territory buttons">
+            <b-button class="recently-worked-button" variant="disabled" v-if="terr.status === 'Recently Worked'" v-b-tooltip.hover title="Recently Worked">
+              <font-awesome-icon icon="ban" />
+            </b-button>
+            <b-button variant="info" v-if="terr.status==='Available'" @click="checkoutTerritory(terr)">check out</b-button>
+            <b-button variant="outline-info" v-if="terr.status==='Checked Out'" @click="checkinTerritory(terr)">check in</b-button>
+          </div>
+        </div>
       </b-list-group-item>
     </b-list-group>
   </div>
@@ -33,10 +42,11 @@ export default {
   data() {
     return {
       congId: 1,
+      publisherId: 43,
       groupCode: '',
       territories: [],
       cities: [],
-      availability: 'All',
+      availability: '',
       availabilityFilters: [
         'All',
         'Available',
@@ -54,9 +64,25 @@ export default {
           'Content-Type': 'application/json'
         },
         data: {
-          query: `{ territories (congId: ${this.congId}, group_code: "${this.groupCode}") { id name type city status }}`
+          query: `query TerritoriesByCongAndGroup($congId: Int $groupCode: String) { 
+            territories (congId: $congId, group_code: $groupCode) { 
+              id 
+              name 
+              type 
+              city 
+              status 
+            }
+          }`,
+          variables: {
+            congId: this.congId,
+            groupCode: this.groupCode,
+          }
         }
       });
+
+      if (!response || !response.data || !response.data.data || !response.data.data.territories) {
+        return null;
+      }
 
       if (this.availability === 'All') {
         return response.data.data.territories;
@@ -69,12 +95,60 @@ export default {
       this.availability = value;
       this.territories = await this.getTerritories();
       sessionStorage.setItem('availability', value);
+    },
+
+    async checkoutTerritory(territory) {
+      await axios({
+        url: 'http://api.foreignfield.com/graphql',
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        data: {
+          query: `mutation CheckoutTerritory($terrId: Int!, $pubId: Int!, $user: String) { 
+            checkoutTerritory(territoryId: $terrId, publisherId: $pubId, user: $user) { 
+              status 
+            }
+          }`,
+          variables: {
+            terrId: territory.id,
+            pubId: this.publisherId,
+            user: territory.proxyUser
+          }
+        }
+      });
+      
+      territory.status = 'Checked Out';
+    },
+
+    async checkinTerritory(territory) {
+      await axios({
+        url: 'http://api.foreignfield.com/graphql',
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        data: {
+          query: `mutation CheckinTerritory($terrId: Int!, $pubId: Int!, $user: String) { 
+            checkinTerritory(territoryId: $terrId, publisherId: $pubId, user: $user) { 
+              status 
+            }
+          }`,
+          variables: {
+            terrId: territory.id,
+            pubId: this.publisherId,
+            user: territory.proxyUser
+          }
+        }
+      });
+
+      territory.status = 'Recently Worked';
     }
   },
 
   async mounted() {
     this.groupCode = this.$route.params.group;
-    this.availability = sessionStorage.getItem('availability');
+    this.availability = sessionStorage.getItem('availability') || 'Available';
     this.territories = await this.getTerritories();
   },
 }
@@ -101,8 +175,21 @@ li {
 }
 .list-group-item {
   text-align: left;
+  padding: 0.75rem 2rem;
 }
-
+.list-group-item:hover {
+  background-color: #f8f9fa;
+  cursor: pointer;
+}
+.list-group-item a {
+  padding-top: 0.4rem;
+}
+.list-group-item h5 {
+  font-weight: normal;
+}
+.recently-worked-button {
+  background: transparent;
+}
 .dropdown-item.active, .dropdown-item:active {
   padding-left: 0;
 }
--- src/main.js
+++ src/main.js
@@ -7,10 +7,10 @@ import { store } from './store';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCheck } from '@fortawesome/free-solid-svg-icons'
+import { faCheck, faBan } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faCheck);
+library.add(faCheck, faBan);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
