73655cfdd000d5cb7532f2979e1624657430227d
Ryan Tercias <ryan.tercias@Ryans-MacBook-Pro-3.local>
2018-08-18 16:37:35 -0400

    changes logic to allow recently worked territories to be checked out again, with warning label

--- src/components/TerritoryCard.vue
+++ src/components/TerritoryCard.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="row justify-content-between">
-    <b-link :to="`/territories/${groupCode}/${terr.id}`" class="w-75">
-      <h5>{{terr.name}}<span v-if="terr.city"> - {{terr.city}}</span></h5>
-    </b-link>
+    <div class="row w-75 align-items-center">
+      <b-link :to="`/territories/${groupCode}/${terr.id}`" class="pr-4">
+        <h5 class="mb-0">{{terr.name}}<span v-if="terr.city"> - {{terr.city}}</span></h5>
+      </b-link>
+      <b-badge class="h-50" variant="warning" v-if="terr.status === 'Recently Worked'">Recently Worked</b-badge>
+    </div>
     <div class="btn-group w-25 row justify-content-end pr-3" role="group" aria-label="Territory buttons">
       <b-btn v-b-modal.checkoutModal variant="info" v-if="terr.status==='Available'" @click="selectTerritory(terr)">check out</b-btn>
-      <b-button variant="outline-info" v-if="terr.status==='Checked Out'" @click="checkinTerritory(terr)">check in</b-button>
-      <b-button class="recently-worked-button" variant="disabled" v-if="terr.status === 'Recently Worked'" v-b-tooltip.hover.boundary.viewport title="Recently Worked">
-        <font-awesome-icon icon="ban" />
-      </b-button>
+      <b-button variant="outline-info" v-if="terr.status === 'Checked Out' || terr.status === 'Recently Worked'" @click="checkinTerritory(terr)">check in</b-button>
     </div>
   </div>
 </template>
