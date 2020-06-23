5bd75a536c4160582b75c58fbfa4f8c2639bc603
Ryan Tercias <ryan.tercias@MacBook-Pro-2.local>
2020-06-23 08:41:16 -0400

    fixes bugs on AddressCard: incomplete slide, undef status, decoupled last activity

--- src/store/modules/address.js
+++ src/store/modules/address.js
@@ -140,7 +140,7 @@ export const address = {
         return current;
       }
 
-      return null;
+      return { value: 'START', timestamp: '' };
     },
 
     checkoutInfo: (state, getters, rootState, rootGetters) => {
