f9221421d493b8a078657febad955877d0887df5
Ryan Tercias <ryan.tercias@MacBook-Pro-2.local>
2020-08-14 17:45:35 -0400

    adds loading indicator on address links

--- src/components/AddressLinks.vue
+++ src/components/AddressLinks.vue
@@ -1,48 +1,62 @@
 <template>
   <div class="address-links">
-    <div class="address-header justify-content-around align-items-center pt-3 pb-3">
-      <div class="lead font-weight-bold w-100 pl-5">
-        <div>{{address.addr1}} {{address.addr2}}</div>
-        <div>{{address.city}} {{address.state_province}} {{address.postalCode}}</div>
+    <Loading v-if="isLoading"></Loading>
+    <div v-else>
+      <div class="address-header justify-content-around align-items-center pt-3 pb-3">
+        <div class="lead font-weight-bold w-100 pl-5">
+          <div>{{address.addr1}} {{address.addr2}}</div>
+          <div>{{address.city}} {{address.state_province}} {{address.postalCode}}</div>
+        </div>
+        <b-link class="pr-4" :to="`/territories/${group}/${territoryId}/addresses/${address.id}/edit`">
+          <font-awesome-icon class="button" icon="edit"></font-awesome-icon>
+        </b-link>
       </div>
-      <b-link class="pr-4" :to="`/territories/${group}/${territoryId}/addresses/${address.id}/edit`">
-        <font-awesome-icon class="button" icon="edit"></font-awesome-icon>
-      </b-link>
+      <div class="lead border-top pt-3 pb-3">Link to...</div>
+      <b-list-group>
+        <b-list-group-item class="lead p-4 font-weight-bold w-auto" :href="mapsUrl" variant="primary" target="_blank">
+          <font-awesome-icon class="button" icon="directions"></font-awesome-icon>&nbsp;
+          Google Maps
+        </b-list-group-item>
+        <b-list-group-item class="lead p-4 font-weight-bold w-auto" variant="warning" :href="lookupFastPeopleSearch">
+          <font-awesome-icon class="button" icon="phone-alt"></font-awesome-icon>&nbsp;
+          Fast People Search
+        </b-list-group-item>
+        <b-list-group-item class="lead p-4 font-weight-bold w-auto" :href="lookup411" variant="success">
+          <font-awesome-icon class="button" icon="phone-alt"></font-awesome-icon>&nbsp;
+          411.com
+        </b-list-group-item>
+        <b-list-group-item
+          class="lead p-4 font-weight-bold w-auto"
+          :to="`/territories/${group}/${territoryId}`"
+          variant="light">
+          Cancel
+        </b-list-group-item>
+      </b-list-group>
     </div>
-    <div class="lead border-top pt-3 pb-3">Link to...</div>
-    <b-list-group>
-      <b-list-group-item class="lead p-4 font-weight-bold w-auto" :href="mapsUrl" variant="primary" target="_blank">
-        <font-awesome-icon class="button" icon="directions"></font-awesome-icon>&nbsp;
-        Google Maps
-      </b-list-group-item>
-      <b-list-group-item class="lead p-4 font-weight-bold w-auto" variant="warning" :href="lookupFastPeopleSearch">
-        <font-awesome-icon class="button" icon="phone-alt"></font-awesome-icon>&nbsp;
-        Fast People Search
-      </b-list-group-item>
-      <b-list-group-item class="lead p-4 font-weight-bold w-auto" :href="lookup411" variant="success">
-        <font-awesome-icon class="button" icon="phone-alt"></font-awesome-icon>&nbsp;
-        411.com
-      </b-list-group-item>
-      <b-list-group-item
-        class="lead p-4 font-weight-bold w-auto"
-        :to="`/territories/${group}/${territoryId}`"
-        variant="light">
-        Cancel
-      </b-list-group-item>
-    </b-list-group>
   </div>
 </template>
 
 <script>
 import { mapGetters, mapActions } from 'vuex';
 import get from 'lodash/get';
+import Loading from './Loading';
 
 export default {
   name: 'AddressLinks',
   props: ['addressId', 'group', 'territoryId'],
+  components: {
+    Loading,
+  },
+  data() {
+    return {
+      isLoading: false,
+    };
+  },
   async mounted() {
+    this.isLoading = true;
     this.setLeftNavRoute(`/territories/${this.group}/${this.territoryId}`);
     await this.fetchAddress(this.addressId);
+    this.isLoading = false;
   },
   computed: {
     ...mapGetters({
