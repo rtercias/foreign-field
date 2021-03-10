0fb0d216455de19daa05f73f8b239b486c382c64
rtercias <ryan.tercias@gmail.com>
2021-03-09 19:13:19 -0500

    add address, phone, and filter counts (#281)

--- src/components/GroupsSelect.vue
+++ src/components/GroupsSelect.vue
@@ -8,12 +8,15 @@
       :to="{ name: 'group', params: { groupId: group.id } }"
       class="mx-0 pl-2 w-100 d-block"
       @click="selectGroup">
-      <font-awesome-icon class="ml-n4" icon="check" v-if="group.id === selectedGroup.id" /> {{group.code}}
+      <font-awesome-icon class="ml-n4" icon="check" v-if="group.id === selectedGroup.id" />
+      {{group.code}}
+      <span v-if="terrCount(group.id)">{{`(${terrCount(group.id)})`}}</span>
     </b-dropdown-item>
   </b-dropdown>
 </template>
 <script>
 import { mapGetters, mapActions } from 'vuex';
+import get from 'lodash/get';
 import truncate from 'lodash/truncate';
 
 export default {
@@ -27,11 +30,19 @@ export default {
     selectGroup(item) {
       this.getGroup({ id: item });
     },
+    terrCount(groupId) {
+      const terr = get(this, 'territories');
+      if (groupId === 0) {
+        return terr.length;
+      }
+      return terr.filter(t => t.group_id === groupId).length;
+    },
   },
   computed: {
     ...mapGetters({
       groups: 'group/groups',
       group: 'group/group',
+      territories: 'territories/territories',
       isDesktop: 'auth/isDesktop',
     }),
     groupsList() {
