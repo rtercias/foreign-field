7c06e58662eadba897ac5b6129144d23e9c249c9
Ryan Tercias <ryan.tercias@gmail.com>
2021-12-11 21:11:36 -0500

    add loginHelperAccess option

--- src/store/modules/models/CongDefaultOptions.js
+++ src/store/modules/models/CongDefaultOptions.js
@@ -1,5 +1,13 @@
 export const CongDefault = {
   options: {
+    publishers: {
+      loginHelperAccess: {
+        options: [
+          { value: null, text: 'Servants and elders only' },
+          { value: 'RP-E', text: 'Regular Pioneers (Enhanced)' },
+        ],
+      },
+    },
     territories: {
       defaultSort: {
         options: [
