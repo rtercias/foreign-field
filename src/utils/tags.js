fa515a1eeb779794e0e90ff7e54cec4c4464f465
Ryan Tercias <ryan.tercias@gmail.com>
2022-05-08 10:02:35 -0400

    fix null error on tags

--- src/utils/tags.js
+++ src/utils/tags.js
@@ -28,13 +28,13 @@ export function formatLanguage(tag = '', value) {
   return String(tag).replaceAll('#language#', value);
 }
 
-export function addTag(notes = '', tag) {
-  const tags = notes.split(',') || [];
+export function addTag(notes, tag) {
+  const tags = (notes || '').split(',') || [];
   if (!tags.includes(tag)) tags.push(tag);
   return tags.join(',');
 }
 
-export function removeTag(notes = '', tag) {
-  const tags = notes.split(',') || [];
+export function removeTag(notes, tag) {
+  const tags = (notes || '').split(',') || [];
   return tags.filter(t => !t.includes(tag)).join(',');
 }
