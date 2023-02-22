969448e2d3bca78c0fb3676a200473f28d520a3f
Ryan Tercias <ryan.tercias@gmail.com>
2023-02-22 01:45:56 -0500

    fix google sso authentication

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -1,17 +1,39 @@
 <template lang="html">
   <div>
     <div v-if="isVerifying" class="px-5 pt-4">
-      <div>A sign-in email with additional instructions was sent to {{email}}. Check your email to complete sign-in.</div>
+      <div>
+        A sign-in email with additional instructions was sent to {{ email }}. Check your
+        email to complete sign-in.
+      </div>
       <b-button @click="reload">Reload</b-button>
     </div>
     <div class="p-5">
-      <b-button class="btn-google" @click="googleLogin">Sign in with Google</b-button>
-      <div v-show="!isVerifying && !isAuthenticated" id="firebaseui-auth-container"></div>
+      <GoogleLogin
+        v-show="!isFirebaseSigninOpen"
+        class="border-light bg-white"
+        :params="params"
+        :onSuccess="googleLogin"
+      >
+        <img
+          src="../assets/g.svg"
+          width="22"
+          height="22"
+        />
+        <span>Sign in with Google</span>
+      </GoogleLogin>
+      <div
+        ref="firebaseUI"
+        v-show="!isVerifying && !isAuthenticated"
+        id="firebaseui-auth-container"
+        @click="isFirebaseSigninOpen = !isFirebaseSigninOpen"
+      >
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import GoogleLogin from 'vue-google-login';
 import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 import firebaseCompat from 'firebase/compat/app';
 import * as firebaseui from 'firebaseui';
@@ -20,12 +42,25 @@ import { mapGetters, mapActions } from 'vuex';
 
 export default {
   name: 'auth',
+  components: {
+    GoogleLogin,
+  },
+  data() {
+    return {
+      isFirebaseSigninOpen: false,
+    };
+  },
   computed: {
     ...mapGetters({
       isAuthenticated: 'auth/isAuthenticated',
       isVerifying: 'auth/isVerifying',
       email: 'auth/email',
     }),
+    params() {
+      return {
+        client_id: process.env.VUE_APP_CLIENT_ID,
+      };
+    },
   },
   methods: {
     ...mapActions({
@@ -34,7 +69,7 @@ export default {
     reload() {
       this.$router.go();
     },
-    onSignInSuccess: ({ user }) => {
+    onSignInSuccess: async ({ user }) => {
       if (user.email && !user.emailVerified) {
         this.verify(user);
       }
@@ -47,59 +82,24 @@ export default {
     async googleLogin() {
       const auth = getAuth();
       const provider = new GoogleAuthProvider();
-      try {
-        const result = await signInWithPopup(auth, provider);
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        // const credential = GoogleAuthProvider.credentialFromResult(result);
-        // const token = credential.accessToken;
-        this.onSignInSuccess(result);
-      } catch (error) {
-        // Handle Errors here.
-        // const errorCode = error.code;
-        // const errorMessage = error.message;
-        // The email of the user's account used.
-        // const email = error.customData.email;
-        // The AuthCredential type that was used.
-        // const credential = GoogleAuthProvider.credentialFromError(error);
-      }
+      const result = await signInWithPopup(auth, provider);
+      this.onSignInSuccess(result);
     },
-    // async emailAndPasswordLogin() {
-    //   try {
-    //     const userCredential = await createUserWithEmailAndPassword(auth, email, password)
-    //     const user = userCredential.user;
-    //     // ...
-    //   } catch(error) {
-    //     const errorCode = error.code;
-    //     const errorMessage = error.message;
-    //     // ..
-    //   }
-    // },
-    // async phoneNumberLogin(phoneNumber, verifier) {
-    //   try {
-    //     const result = await signInWithPhoneNumber(auth, phoneNumber, verifier)
-    //     // SMS sent. Prompt user to type the code from the message, then sign the
-    //     // user in with result.confirm(code).
-    //     // result.confirm(code);
-    //     // ...
-    //   } catch(error) {
-    //     // Error; SMS not sent
-    //     // ...
-    //   }
-    // },
-    // recaptchaVerify() {
-    //   const verifier = new RecaptchaVerifier('sign-in-button', {
-    //     size: 'invisible',
-    //     callback: (response) => {
-    //       // reCAPTCHA solved, allow signInWithPhoneNumber.
-    //       this.phoneNumberLogin(verifier);
-    //     }
-    //   }, auth);
-    // },
   },
   mounted() {
+    // const observer = new MutationObserver(() => {
+    //   const firebaseUI = get(this.$refs, 'firebaseUI');
+    //   const header = firebaseUI && firebaseUI.querySelector('.firebaseui-card-header');
+    //   const style = header && window.getComputedStyle(header);
+    //   // eslint-disable-next-line
+    //   console.log('style display', style ? style.display : false);
+    //   this.isFirebaseSigninOpen = style ? style.display === 'block' : false;
+    // });
+
+    // observer.observe(this.$refs.firebaseUI, { childList: true });
+
     const uiConfig = {
       signInOptions: [
-        // firebaseCompat.auth.GoogleAuthProvider.PROVIDER_ID,
         firebaseCompat.auth.EmailAuthProvider.PROVIDER_ID,
         firebaseCompat.auth.PhoneAuthProvider.PROVIDER_ID,
       ],
@@ -119,9 +119,22 @@ export default {
   },
 };
 </script>
-<style>
-.btn-google {
+<style lang="scss">
+#google-signin-btn-0 {
   width: 200px;
   border-radius: 0;
+  font-size: 14px;
+  min-height: 40px;
+
+  img {
+    margin-left: -12px;
+  }
+  span {
+    margin-left: 12px;
+  }
+  .mdl-card {
+    margin-top: -40px;
+  }
 }
+
 </style>
