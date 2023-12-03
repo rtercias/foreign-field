2c9f3e21c2dec64dcc2982e1b5c6025a203c9608
Ryan Tercias <ryan.tercias@ryans-mbp-3.mynetworksettings.com>
2023-12-03 15:28:05 -0500

    chore: use single quotes on eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,51 +1,51 @@
 console.log('env', process.env.NODE_ENV);
 module.exports = {
-  "root": true,
-  "extends": [
-    "plugin:vue/base",
-    "@vue/airbnb"
+  root: true,
+  extends: [
+    'plugin:vue/base',
+    '@vue/airbnb',
   ],
-  "env": {
-    "node": true
+  env: {
+    node: true,
   },
-  "parserOptions": {
-    "parser": "babel-eslint",
-    "sourceType": "module",
-    "allowImportExportEverywhere": false,
-    "codeFrame": false,
-    "ecmaVersion": 2017
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module',
+    allowImportExportEverywhere: false,
+    codeFrame: false,
+    ecmaVersion: 2017,
   },
-  "rules": {
-    "max-len": [2, { "code": 125 }],
-    "comma-dangle": [
-      "error",
-      "always-multiline"
+  rules: {
+    'max-len': [2, { code: 125 }],
+    'comma-dangle': [
+      'error',
+      'always-multiline',
     ],
-    "import/prefer-default-export": "off",
-    "no-await-in-loop": "off",
-    "no-restricted-syntax": [
-      "error",
-      "LabeledStatement",
-      "WithStatement"
+    'import/prefer-default-export': 'off',
+    'no-await-in-loop': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
     ],
-    "no-console": process.env.NODE_ENV === 'production' ? 2 : 0,
-    "no-debugger": process.env.NODE_ENV === 'production' ? 2 : 0,
-    "no-param-reassign": [
-      "error",
+    'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-param-reassign': [
+      'error',
       {
-        "props": false
-      }
+        props: false,
+      },
     ],
-    "no-return-assign": "off",
-    "no-mixed-operators": "off",
-    "import/no-cycle": "off",
-    "import/extensions": "off",
-    "import/order": "off",
-    "no-alert": "off",
-    "no-plusplus": "off",
-    "func-names": "off",
-    "linebreak-style": "off",
-    "object-curly-newline": "off",
-    "guard-for-in": "off"
-  }
-}
+    'no-return-assign': 'off',
+    'no-mixed-operators': 'off',
+    'import/no-cycle': 'off',
+    'import/extensions': 'off',
+    'import/order': 'off',
+    'no-alert': 'off',
+    'no-plusplus': 'off',
+    'func-names': 'off',
+    'linebreak-style': 'off',
+    'object-curly-newline': 'off',
+    'guard-for-in': 'off',
+  },
+};
